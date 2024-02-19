b5b31368f62ad6906e4c9d6f048826566521668c
Max Ivashchenko <maxiivashenko@yandex.ru>
2024-02-19 17:22:02 +0300

    Pattern matching done

---
+++ PatternMatching/Program.cs
@@ -0,0 +1,76 @@
+﻿namespace PatternMatching;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Employee tom = new Manager();
+        tom.Work();
+        UseEmployee(tom);
+
+        Employee? bob = new Employee();
+        Employee? john = null;
+
+        NullCheck(bob);
+        NullCheck(john);
+
+        UseSwitch(tom);
+        UseSwitch(bob);
+        UseSwitch(john);
+
+
+        Console.ReadKey();
+
+    }
+
+    static void UseSwitch(Employee? emp)
+    {
+        switch (emp)
+        {
+            case Manager manager when !manager.IsOnVacation:
+                manager.Work();
+                break;
+            case null:
+                Console.WriteLine("obj is null");
+                break;
+            default:
+                Console.WriteLine("Object is not Manager");
+                break;
+        }
+    }
+    static void NullCheck(Employee? emp)
+    {
+        if (emp is not null)
+        {
+            emp.Work();
+        }
+        else
+        {
+            Console.WriteLine("Not working");
+        }
+    }
+
+    static void UseEmployee(Employee emp)
+    {
+        if (emp is Manager manager && manager.IsOnVacation == false)
+        {
+            manager.Work();
+        }
+        else
+        {
+            Console.WriteLine("Not working");
+        }
+    }
+}
+
+public class Employee
+{
+    public virtual void Work() => Console.WriteLine("Emploeey working");
+
+}
+
+public class Manager : Employee
+{
+    public override void Work() => Console.WriteLine("Manager working");
+    public bool IsOnVacation { get; set; }
+}  
---
+++ PatternMatchingProperties/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace PatternMatchingProperties;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Person tom = new Person { Language = "English", Status = "user", Name = "Tom" };
+        Person pierre = new Person { Language = "French", Status = "user", Name = "Pierre" };
+        Person tomas = new Person { Language = "German", Status = "Admin", Name = "Tomas" };
+        Person pablo = new Person { Language = "Spanish", Status = "user", Name = "Pablo" };
+
+        SayHello(tom); 
+        Console.WriteLine(GetMessage(tomas));
+        Console.WriteLine(GetMessage(pierre));
+        Console.WriteLine(GetMessage(pablo));
+
+        Console.ReadKey();
+    }
+
+    static string GetMessage(Person? person)
+    {
+        return person switch
+        {
+            { Language: "English" } => "Hello",
+            { Language: "French" } => "Bonjour",
+            { Language: "German", Status: "Admin" } => "Hallo, admin!",
+            null => "NULL",
+            { } => "undefined"
+        };
+    }
+
+    static void SayHello(Person? person)
+    {
+        if (person is Person { Language: "English" })
+        {
+            Console.WriteLine("Hello!");
+        }
+        else
+        {
+            Console.WriteLine("Fuck, off");
+        }
+    }
+}
+
+public class Person
+{
+    public string Name { get; set; } = "";
+    public string Status { get; set; } = "";
+    public string Language { get; set; } = "";
+}
